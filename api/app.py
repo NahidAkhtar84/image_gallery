17bcdc8033341f58a92aa36bdc7b8f5adfa9f227
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-08-12 01:39:01 +0600

    enabled python lint and black code formatter

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -10,11 +11,15 @@ UNSPLASH_KEY = os.environ.get("REACT_APP_UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("please create .env.local file and set your valid secret UNSPLASH_KEY there with a name of the variable REACT_APP_UNSPLASH_KEY")
+    raise EnvironmentError(
+        "please create .env.local file and set your valid secret UNSPLASH_KEY there with a name of the variable REACT_APP_UNSPLASH_KEY"
+    )
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG
+
 
 @app.route("/new-image")
 def new_image():
@@ -22,14 +27,13 @@ def new_image():
 
     headers = {
         "Accept-Version": "v1",
-        "Authorization": "Client-ID {}".format(UNSPLASH_KEY)
+        "Authorization": "Client-ID {}".format(UNSPLASH_KEY),
     }
-    params = {
-        "query": word
-    }
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Wecome from './components/Welcome';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 
 function App() {
   const [word, setWord] = useState('');
@@ -17,9 +17,7 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
-    )
+    fetch(`${API_URL}/new-image?query=${word}`)
       .then((response) => response.json())
       .then((data) => {
         setImages([{ ...data, title: word }, ...images]);
